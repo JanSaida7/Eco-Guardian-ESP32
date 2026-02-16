7b2cff4df8cb46d6386419c4e464dbef0616a20b
Jan <jansaidasyed743@gmail.com>
2026-02-16 23:25:26 +0530

    Update utils.py with all shared constants

--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,4 @@
+import os
 
 DATA_DIR = "data"
 CLASSES = ["background", "chainsaw", "gunshot"]
@@ -7,3 +8,10 @@ DURATION = 2.0
 METADATA_URL = "https://raw.githubusercontent.com/karolpiczak/ESC-50/master/meta/esc50.csv"
 AUDIO_BASE_URL = "https://raw.githubusercontent.com/karolpiczak/ESC-50/master/audio/"
 METADATA_FILENAME = "esc50.csv"
+
+N_MELS = 64
+N_FFT = 1024
+HOP_LENGTH = 512
+MODEL_DIR = "models"
+INPUT_SHAPE = (64, 63, 1)
+SYNTHETIC_DIR = os.path.join(DATA_DIR, "synthetic_train")
